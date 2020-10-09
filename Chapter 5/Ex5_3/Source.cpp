5a24b79f070685343e83558db251127d9b5eb3e8
Genghis Khan <gkomet@g.jct.ac.il>
2020-10-09 15:07:31 +0300

    Corrected and refactored 5.3

--- Chapter 5/Ex5_3/Source.cpp
+++ Chapter 5/Ex5_3/Source.cpp
@@ -3,21 +3,23 @@
 
 #define EXE_FILENAME "SinglePhilosopher\\Debug\\SinglePhilosopher.exe"
 #define NUM_PROCESSES 5
+constexpr INT size = 48; // length is increased by 3:
+// space character- 1 byte,
+	// one digit number- 1 byte,
+	// string NULL terminator- 1 byte
+constexpr INT length_mutex_name = 11; // strlen(Chopstick0) + null
 
 int main()
 {
-	HANDLE hPhilosophers[NUM_PROCESSES];// = CreateMutexA(NULL, FALSE, "Hello");
-	char mutex_name[11];
+	HANDLE hPhilosophers[NUM_PROCESSES];
+	char mutex_name[length_mutex_name];
 
 	for (size_t i = 0; i < NUM_PROCESSES; i++)
 	{
-		sprintf_s(mutex_name, 11, "Chopstick%zu", i);
+		sprintf_s(mutex_name, length_mutex_name, "Chopstick%zu", i);
 		hPhilosophers[i] = CreateMutexA(NULL, FALSE, mutex_name);
 	}
-	constexpr INT size = 48; // length is increased by 3:
-	// space character- 1 byte,
-	// one digit number- 1 byte,
-	// string NULL terminator- 1 byte
+
 	char param[size];
 	STARTUPINFOA si;
 	PROCESS_INFORMATION pi[NUM_PROCESSES];
