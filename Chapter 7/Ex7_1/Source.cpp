2ec5d7d2b1ac4a193cd62de267e6e5778ae9a99f
Genghis Khan <gkomet@g.jct.ac.il>
2020-10-10 22:41:10 +0300

    Good night. Going to debug 7.1 tomorrow

--- Chapter 7/Ex7_1/Source.cpp
+++ Chapter 7/Ex7_1/Source.cpp
@@ -3,5 +3,46 @@
 
 int main()
 {
+	HANDLE hFile = CreateFile(TEXT("gibrish.bin"),
+		GENERIC_READ,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	char read_char;
+	DWORD chars_read;
+	int As = 0;
+	BOOL success = false;
+
+	do
+	{
+		if (FALSE != ReadFile(hFile,
+			&read_char,
+			1,
+			&chars_read,
+			NULL))
+		{
+			if (read_char == 'A')
+			{
+				As++;
+			}
+		}
+
+		else
+		{
+			success = true;
+		}
+
+		//printf("%c", read_char);
+
+
+	} while (!success);
+
+	printf("%d\n", As);
+
+
+
 	return 0;
 }
